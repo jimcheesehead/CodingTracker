7a09a1c3e677f7cf7b5b272fd917f129c312ee5d
jimcheesehead <jim.gregor@gmailcom>
2024-03-10 10:47:44 -0400

    Nowwe have many messages

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Fuck Joe Biden!");
+Console.WriteLine("Fuck Joe Biden!");
+Console.WriteLine("Fuck Joe Biden!");
+Console.WriteLine("Fuck Joe Biden!");
